cab6b2ab85fa5470736b6c1fbf09f2ad7c8740e1
yneo918 <neo.yuichiro@megachips.co.jp>
2024-10-31 16:46:23 -0700

    delete dirs

--- src/build/gps_target_push/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/rsl/pioneerbase_ws/src/install/gps_target_push'
--- src/build/mode_select/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/rsl/pioneerbase_ws/src/install/mode_select'
